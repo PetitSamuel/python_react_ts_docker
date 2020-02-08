cc9e4c5b7951b1263ab8ddd7ba09b3a9b4073a35
PetitSamuel <petits@tcd.ie>
2020-02-08 19:34:55 +0000

    messing around

--- backend/app.py
+++ backend/app.py
@@ -13,8 +13,23 @@ def hello():
     return 'This Compose/Flask demo has been viewed %s time(s).' % redis.get('hits')
 
 @app.route('/test')
-def hey():
+def test():
     return jsonify(result="hello my test!")
+
+@app.route('/session/<id>')
+def session(id):
+    print(id)
+    return jsonify(
+        sessionInfo="Welcome to session %s" % id,
+        other="cool"
+    )
+@app.route('/set_session/<id>')
+def incrSession(id):
+    redis.incr("%s" % id)
+    print(id)
+    print('number: %s times' % redis.get('%s' % id))
+    return 'this sessions has been viewed %s time(s).' % redis.get('%s' % id)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,41 @@
 import React from 'react';
 import './App.css';
-import image from './img/nath.jpg'; 
+import image from './img/nath.jpg';
 
 const App = () => {
   return (
     <div className="App">
       <div className="App-header">
-      <div id="side-bar">
-        <div id="line">
-          <img src={image} />
-          <p>column lol</p>
+        <div id="side-bar">
+          <div id="line">
+            <img src={image} alt="header-img" />
+            <p>Samuel Petit</p>
+          </div>
         </div>
-      </div>
-      <div id="main">
-        <p>column lol</p>
-      </div>
+        <div id="main">
+          <p>Some stuff here :)</p>
+          <button type="submit" onClick={handleInputSubmit}>click me</button>
+        </div>
       </div>
     </div>
   );
 }
-//      <button onClick={test}> Hey ! </button>
-
 
 export default App;
 
+function testt() {
+  console.log("change lel");
+}
 async function test() {
   fetch(`http://localhost:5000/test`)
-      .then(res => res.json())
-      .then(res => {
-          console.log(res)
-      });
-} 
+    .then(res => res.json())
+    .then(res => {
+      console.log(res)
+    });
+}
+
+function handleInputSubmit() {
+  var input = document.getElementById('show-tooltip');
+  console.log(input);
+  
+}
