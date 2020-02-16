5ab525979c354403177e27bdc6ac590d0e4f65b2
PetitSamuel <petits@tcd.ie>
2020-02-16 04:26:18 +0000

    polishing changes from prev commit

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,9 @@
-# compose_flask/app.py
 import os
 from os import environ
 from datetime import datetime
 from flask import Flask, Response, request, jsonify, render_template
 from redis import Redis
 from flask_cors import CORS
-
 import models
 from models import db, User
 
@@ -13,7 +11,7 @@ app = Flask(__name__)
 redis = Redis(host='redis', port=6379)
 CORS(app)
 
-# Configuration is provided through environment variables by Docker Compose
+# Configuration from environment variables by Docker Compose
 app.config.update({
     'SQLALCHEMY_TRACK_MODIFICATIONS': False,
     'SECRET_KEY': environ['FLASK_SECRET'],
@@ -25,9 +23,6 @@ app.config.update({
     # Database 1 is for background tasks
     #'CELERY_RESULT_BACKEND': 'redis://redis:6379/1',
     #'CELERY_BROKER_URL': 'redis://redis:6379/1',
-})
-
-app.config.update({
     'SQLALCHEMY_DATABASE_URI': 'mysql+mysqlconnector://{user}:{password}@{host}/{database}'.format(
         user = environ['MYSQL_USER'],
         password = environ['MYSQL_PASSWORD'],
@@ -36,30 +31,34 @@ app.config.update({
     ),
 })
 
+models.init_app(app)
+
 @app.errorhandler(404)
 def not_found(e):
     if request.path.startswith('/api/v1'):
         return jsonify({'message': 'Not found'}), 404
-
-    return '404 : not found'
-
-models.init_app(app)
+    return '404 : not found', 404
 
 @app.route('/')
 def hello():
     redis.incr('hits')
-    return 'This Compose/Flask demo has been viewed %s time(s).' % redis.get('hits')
+    user = User.find_by_username('root')
+    return jsonify({
+        'user': user.id,
+        'username': user.username,
+    })
 
 @app.before_first_request
 def init_db():
-    # Create the tables (does nothing if they already exist)
+    # Create tables (does nothing if they already exist)
     db.create_all()
 
-    # Create the default `root` user if they don't exist
+    # check root exists
+    # todo : create if doesn't exist
     root = User.find_by_username('root')
     if not root:
         print('**** NO ROOT USER FOUND ****')
-    print(root)
+    print('*** ROOT USER FOUND ***')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
